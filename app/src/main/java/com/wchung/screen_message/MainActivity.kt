493bd0e7f9a8c3cd9fe1e1e4882ab4267c7ee23a
William <wiiliamchung360@gmail.com>
2025-02-11 22:38:03 -0800

    Mirror the textview with the text in editview

--- app/src/main/java/com/wchung/screen_message/MainActivity.kt
+++ app/src/main/java/com/wchung/screen_message/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.wchung.screen_message
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var message : EditText? = null
+    var screen : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        message = findViewById(R.id.message)
+        screen = findViewById(R.id.screen)
+
+        message?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                screen?.text = s.toString()
+                // you can call or do what you want with your EditText here
+                // yourEditText...
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+        })
     }
 }
